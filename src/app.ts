934b3a52d3d0ae799387e9d50e6c2356ce649ebd
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2022-04-03 15:11:40 -0300

    Add dockerfile

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,13 @@ const env = process.env;
 const PORT = env.PORT || 3002;
 const app = express();
 
+app.get('/api/status', (req, res) => {
+  return res.json({
+    service: "Sales-api",
+    status: "up",
+    httpStatus: 200
+  })
+})
+
 app.listen(PORT, () => console.info(`Server started at ${PORT}`))
 
